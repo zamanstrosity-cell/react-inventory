b23d687cb45bdbf2d25fb472c74502a0072e5181
Omar Mansour <omarsayedmohamedmansour@gmail.com>
2020-07-05 15:58:42 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,35 @@ import Inventory from './components/Inventory'
 import './App.css';
 
 function App() {
+
+  let state = {
+    inventory: [
+      {
+        id: 1,
+        title: 'Tomatoes',
+        quantity: '45',
+        units: 'kilo'
+      },
+      {
+        id: 2,
+        title: 'Tomatoes',
+        quantity: '45',
+        units: 'kilo'
+      },
+      {
+        id: 3,
+        title: 'Tomatoes',
+        quantity: '45',
+        units: 'kilo'
+      },
+      {
+        id: 4,
+        title: 'Potto',
+        quantity: '45',
+        units: 'kilo'
+      }
+    ]
+  }
   return (
     <div className="App">
       <header className="App-header">
