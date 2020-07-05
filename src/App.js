d8c767c84ded6d1c5f722391162258a55510c0c6
Omar Mansour <omarsayedmohamedmansour@gmail.com>
2020-07-05 15:41:49 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './shelves.png';
+import Inventory from './components/Inventory'
 import './App.css';
 
 function App() {
@@ -7,18 +8,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Inventory System</p>
       </header>
+      <div className="container">
+      <Inventory />
+      </div>
     </div>
   );
 }
---
+++ src/components/Inventory.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Inventory extends React.Component {
+  render() {
+      return (
+        <div className="inventory-container">
+        <h1>Inventory</h1>
+        </div>
+    );
+  }
+}
+
+export default Inventory;
